617683fae03a2fd722c41b42737ef38270ba5058
Maribel <maribel.duron86@gmail.com>
2023-12-26 11:31:28 -0600

    added cors pkg

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require ('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://maribelduron86:U0NQNlpjb3hHRDd4NUhEcQ%3D%3D@cluster0.sceyqpu.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cors());
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://maribelduron86:U0NQNlpjb3hHRDd4NUhEcQ%3D%3D@cluster0.sceyqpu.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
