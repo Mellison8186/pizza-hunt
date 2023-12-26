804c2ff6f289c414fd52ba7ae6e403cb2d692e82
Maribel <maribel.duron86@gmail.com>
2023-12-26 12:52:47 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require(`mongoose`);
+const express = require(`express`);
+const app = express();
 //const cors = require ('cors');
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://maribelduron86:U0NQNlpjb3hHRDd4NUhEcQ%3D%3D@cluster0.sceyqpu.mongodb.net/?retryWrites=true&w=majority";
-
-const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb+srv://maribelduron86:U0NQNlpjb3hHRDd4NUhEcQ%3D%3D@cluster0.sceyqpu.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(`public`));
 //app.use(cors());
 
-app.use(require('./routes'));
+app.use(require(`./routes`));
 
 mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
@@ -21,5 +20,5 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-app.listen(PORT, () => console.log('🌍 Connected on localhost:${PORT}'));
+mongoose.set(`debug`, true);
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
